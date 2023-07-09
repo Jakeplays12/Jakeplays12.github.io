97ae0abf2c9289a3691a076423d1d4433a5c95df
Jakeplays12 <96874701+Jakeplays12@users.noreply.github.com>
2023-07-09 15:31:25 +0100

    LinksUpdate

--- Animations/Fade/Fade.js
+++ Animations/Fade/Fade.js
@@ -1,6 +1,6 @@
 const fadeItems = document.querySelectorAll('.fade-item');
 fadeItems.forEach((item, index) => {
-  item.style.animationDelay = `${index * 0.2}s`;
+  item.style.animationDelay = `${index * 0.1}s`;
   setTimeout(() => {
     item.style.visibility = 'visible';
   }, 100); // Delay in milliseconds before making the element visible
